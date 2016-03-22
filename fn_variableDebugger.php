e660a56c48b98f801c9157cb8f672c808a5ae8a4
janovoas <javiernovoa.s@gmail.com>
2016-03-22 16:38:42 -0300

    Create fn_variableDebugger.php

---
+++ fn_variableDebugger.php
@@ -0,0 +1,14 @@
+// VARIABLE DEBUGGER
+// v1.0
+// Quick variable debugger. Gets data and prints it between "pre" tags. Works with print_r or var_dump (var_dump indicates the data type, print_r doesn't).
+
+// USAGE EXAMPLE 
+// ns_debug($variable)        ---> prints print_r
+// ns_debug($variable, true)  ---> prints var_dump
+
+// FUNCTION
+function ns_debug($data, $dump = NULL)
+{
+    if ( $dump == true ) { echo "<pre>";var_dump($data);echo "</pre>"; }
+    else { echo "<pre>";print_r($data);echo "</pre>"; }
+}
